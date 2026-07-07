983780e2de319115ce33adc81d126a5b91d7e846
Bogdan <118662814+ItsMagisterio@users.noreply.github.com>
2026-07-07 19:51:34 +0300

    Add FlashDevelop project file

--- copy-resources.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-if not exist bin mkdir bin
-if exist resources\cfg.dxt1.xml copy /Y resources\cfg.dxt1.xml bin\cfg.dxt1.xml
-if exist resources\cfg.etc1.xml copy /Y resources\cfg.etc1.xml bin\cfg.etc1.xml
-if exist resources\cfg.pvrtc.xml copy /Y resources\cfg.pvrtc.xml bin\cfg.pvrtc.xml
-if exist resources\data xcopy /E /I /Y resources\data bin\data
